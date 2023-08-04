805806e2de88aa007cabf07e5618f719c12ca0ad
kushagra singh <kushagrasingh04897@gmail.com>
2023-08-04 22:40:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Medical Representative", "AI Enthusiast", "Career Advisor","Blogger"],
+    default: false,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
